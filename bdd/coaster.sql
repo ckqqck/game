1994616f872c3905516194022f143956da07e283
h1fra <bodin.samuel@gmail.com>
2012-10-23 17:40:30 +0200

    Data

--- bdd/coaster.sql
+++ bdd/coaster.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2012 at 04:48 PM
+-- Generation Time: Oct 23, 2012 at 05:39 PM
 -- Server version: 5.5.27-log
 -- PHP Version: 5.4.6
 
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `chunk` (
 CREATE TABLE IF NOT EXISTS `employee` (
   `id_employee` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
-  `id_map` int(11) NOT NULL,
+  `id_chunk` int(11) NOT NULL,
   `pending` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_employee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -104,7 +104,17 @@ CREATE TABLE IF NOT EXISTS `item` (
   `upkeep` int(11) NOT NULL DEFAULT '1000',
   `drop_rate` float(16,6) NOT NULL,
   PRIMARY KEY (`id_item`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`id_item`, `type`, `name`, `price`, `time_to_build`, `item_x`, `item_y`, `min_visit`, `max_visit`, `upkeep`, `drop_rate`) VALUES
+(1, 'flooring', 'herb', 10, 3, '1', '1', 0, 0, 10, 0.000000),
+(2, 'carousel', 'Carousel', 70000, 10, '2', '2', 50, 100, 1000, 0.000000),
+(3, 'shop', 'Hotdog', 20000, 7, '2', '2', 30, 55, 500, 0.000000),
+(4, 'flooring', 'road', 15, 3, '1', '1', 0, 0, 10, 0.000000);
 
 -- --------------------------------------------------------
 
@@ -119,6 +129,13 @@ CREATE TABLE IF NOT EXISTS `level` (
   `reward_amount` int(11) NOT NULL,
   `reward_type` enum('loops','golden','xp') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `level`
+--
+
+INSERT INTO `level` (`id_level`, `gauge`, `ratio`, `reward_amount`, `reward_type`) VALUES
+(1, 10, 1.000000, 100, 'loops');
 
 -- --------------------------------------------------------
 
@@ -136,7 +153,14 @@ CREATE TABLE IF NOT EXISTS `map` (
   `total_visit` int(11) NOT NULL DEFAULT '0',
   `total_gain` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_map`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `map`
+--
+
+INSERT INTO `map` (`id_map`, `id_user`, `name`, `type`, `map_x`, `map_y`, `total_visit`, `total_gain`) VALUES
+(1, 1, 'Awesome Park', 'default', 27, 18, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -170,7 +194,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `score_total` int(11) NOT NULL,
   `level` tinyint(4) NOT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `id_map`, `time_played`, `money`, `golden_ticket`, `score_total`, `level`) VALUES
+(1, 1, 150, 150000, 5, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -184,6 +215,13 @@ CREATE TABLE IF NOT EXISTS `user_quest` (
   `id_user` int(11) NOT NULL,
   `state` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_quest`
+--
+
+INSERT INTO `user_quest` (`id_user_quest`, `id_quest`, `id_user`, `state`) VALUES
+(1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
